1e4d7d0c8860452448a4b753e85a1761d17acced
galobponce <personal.galo.bello@gmail.com>
2023-07-18 22:39:50 -0300

    Add multi-step login form

---
+++ src/routes/login/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<nav class="flex justify-center my-6">
+	<ul class="steps">
+		<a href="/login" class="step step-primary">Log In</a>
+		<a
+			href="/login/username"
+			class="step"
+			class:step-primary={$page.route.id?.match(/username|photo/g)}
+		>
+			Choose Username
+		</a>
+		<a href="/login/photo" class="step" class:step-primary={$page.route.id?.match(/photo/g)}>
+			Upload Photo
+		</a>
+	</ul>
+</nav>
+
+<main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
+	<div class="card-body items-center text-center">
+		<slot />
+	</div>
+</main>
