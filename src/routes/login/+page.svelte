faed8e5e4cab1c57f100be17987f66b01a0ae371
galobponce <personal.galo.bello@gmail.com>
2023-07-20 23:59:17 -0300

    Add simple sign in with google pop up

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,14 @@
 <script lang="ts">
+	import { auth } from '$lib/firebase';
+	import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+	const signInWithGoogle = async () => {
+		const provider = new GoogleAuthProvider();
+		const user = await signInWithPopup(auth, provider);
+		console.log(user);
+	};
 </script>
 
 <h2>Login</h2>
+
+<button class="btn btn-primary" on:click={signInWithGoogle}>Sign In With Google</button>
