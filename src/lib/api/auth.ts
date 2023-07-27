e3969147e01b38cff9d107b9d43aa7b13e621c75
galobponce <personal.galo.bello@gmail.com>
2023-07-27 15:57:33 -0300

    Implement logout function

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
 import { auth } from '../config/firebase';
 
@@ -6,4 +6,8 @@ export const logInWithGoogle = async () => {
 	const provider = new GoogleAuthProvider();
 	const user = await signInWithPopup(auth, provider);
 	console.log(user);
+};
+
+export const logOut = () => {
+	signOut(auth);
 };
