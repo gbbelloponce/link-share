59b037137b857529bfd57e0ce073fa0ac47a98c3
galobponce <personal.galo.bello@gmail.com>
2023-07-27 16:01:51 -0300

    Create component to check authentication

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { user } from '$lib/store/user';
+</script>
+
+{#if $user}
+	<slot />
+{:else}
+	<p class="text-error my-10">
+		You must be logged in to view this page
+		<a class="btn btn-primary" href="/login">Log In</a>
+	</p>
+{/if}
