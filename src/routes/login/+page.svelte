f7dfbcda1269ca48d83907fb737c88c43835a97b
galobponce <personal.galo.bello@gmail.com>
2023-08-04 15:45:46 -0300

    Change button message

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -8,5 +8,5 @@
 	<p class="text-center text-success">You are logged in</p>
 	<button class="btn btn-secondary" on:click={logOut}>Log out</button>
 {:else}
-	<button class="btn btn-primary" on:click={logInWithGoogle}>Sign in with Google</button>
+	<button class="btn btn-primary" on:click={logInWithGoogle}>Log in with Google</button>
 {/if}
