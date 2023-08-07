281f0491d1abc7eabbf032e45b0a5eb0d97bb725
galobponce <personal.galo.bello@gmail.com>
2023-08-06 23:47:06 -0300

    Create user data

---
+++ src/lib/data/userData.ts
@@ -0,0 +1,22 @@
+import { derived, type Readable } from 'svelte/store';
+
+import { genericDocStore } from '../store/genericDocStore';
+import { authFirebaseUserData } from './authFirebaseUserData';
+
+interface UserData {
+	username: string;
+	bio: string;
+	photoURL: string;
+	links: unknown[];
+}
+
+export const userData: Readable<UserData | null> = derived(
+	authFirebaseUserData,
+	($authFirebaseUser, set) => {
+		if ($authFirebaseUser) {
+			return genericDocStore<UserData>(`users/${$authFirebaseUser.uid}`).subscribe(set);
+		} else {
+			set(null);
+		}
+	}
+);
