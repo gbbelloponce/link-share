89f07c97ed3c63b639bcda579f9b58edb4e45f04
galobponce <personal.galo.bello@gmail.com>
2023-08-10 17:05:12 -0300

    Modify styles on user landing

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import UserLink from '$lib/components/UserLink.svelte';
 
 	export let data: PageData;
 </script>
@@ -9,10 +10,18 @@
 	<meta name="description" content={data.bio} />
 </svelte:head>
 
-<main class="prose text-center mx-auto mt-5 flex flex-col items-center gap-4">
+<main class="prose w-full md:max-w-2xl text-center mx-auto mt-5 flex flex-col gap-4">
 	<h1 class="text-3xl font-bold text-purple-500">@{data.username}</h1>
 
 	<img src={data.photoURL} alt="photoURL" width="256" height="256" class="mx-auto rounded-lg" />
 
 	<p class="text-xl">{data.bio ?? 'no bio yet...'}</p>
+
+	<ul class="list-none p-2 mx-1">
+		{#each data.links as link}
+			<li>
+				<UserLink {...link} />
+			</li>
+		{/each}
+	</ul>
 </main>
