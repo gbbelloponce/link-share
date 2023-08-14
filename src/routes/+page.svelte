ac678e164cb426698fe0b2eb4ff1a2a2d8326ad2
galobponce <personal.galo.bello@gmail.com>
2023-08-14 16:45:36 -0300

    Add homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,26 @@
 <script lang="ts">
+	import { userData } from '$lib/data/userData';
 </script>
 
-<h1>Home</h1>
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<main class="flex-1">
+	<div class="hero my-10">
+		<div class="hero-content text-center">
+			<div class="w-full">
+				<h1 class="text-7xl font-bold">Link Share</h1>
+				<p class="py-6 text-2xl">One simple link to share your entire portfolio</p>
+				<div class="flex mx-auto w-full sm:w-4/5 justify-center items-center gap-5">
+					{#if $userData}
+						<a href="/{$userData.username}/edit" class="btn btn-primary">Edit Profile</a>
+					{:else}
+						<a href="/login" class="btn btn-primary flex-1">Log In</a>
+						<a href="/gbbelloponce" class="btn btn-secondary flex-1">Example Profile</a>
+					{/if}
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
