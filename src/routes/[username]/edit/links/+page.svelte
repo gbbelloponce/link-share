309f5aa6b8966ab23ddbfb436a20930278f378a2
galobponce <personal.galo.bello@gmail.com>
2023-08-19 07:32:25 -0300

    Display user links in settings

--- src/routes/[username]/edit/links/+page.svelte
+++ src/routes/[username]/edit/links/+page.svelte
@@ -1,4 +1,16 @@
 <script lang="ts">
+	import { userData } from '$lib/data/userData';
+	import UserLink from '$lib/components/UserLink.svelte';
 </script>
 
-<h2>Links</h2>
+<h2 class="card-title mb-5">Links</h2>
+
+{#if $userData?.links}
+	<ul class="w-full max-w-sm list-none">
+		{#each $userData.links as link}
+			<li class="my-2">
+				<UserLink {...link} />
+			</li>
+		{/each}
+	</ul>
+{/if}
