a2565e6f3a9da9df3681e05792101591ae0391a7
galobponce <personal.galo.bello@gmail.com>
2023-08-25 16:58:43 -0300

    Open link in a new tab

--- src/lib/components/UserLink.svelte
+++ src/lib/components/UserLink.svelte
@@ -6,6 +6,7 @@
 
 <a
 	href={url}
+	target="_blank"
 	class="bg-base-200 no-underline rounded-lg p-3 flex justify-center items-center relative"
 >
 	<img src={`/icons/${icon}.svg`} alt={icon} width="32" height="32" class="w-8 absolute left-3" />
