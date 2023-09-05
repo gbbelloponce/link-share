1cacd36915a5b1729945eb8ac6fe50d5ac8eb75d
Galo Benjamín Bello Ponce <15621798-gbbelloponce1@users.noreply.gitlab.com>
2023-09-05 16:43:03 -0300

    Add firebase credentials

--- src/lib/config/firebase.ts
+++ src/lib/config/firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: process.env.FB_API_KEY,
-	authDomain: process.env.FB_AUTH_DOMAIN,
-	projectId: process.env.FB_PROJECT_ID,
-	storageBucket: process.env.FB_STORAGE_BUCKET,
-	messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-	appId: process.env.FB_APP_ID
+	apiKey: 'AIzaSyD81IWicVk9Danv5ee7O2r593ZHyy7j2mo',
+	authDomain: 'link-share-3ecc8.firebaseapp.com',
+	projectId: 'link-share-3ecc8',
+	storageBucket: 'link-share-3ecc8.appspot.com',
+	messagingSenderId: '667856146107',
+	appId: '1:667856146107:web:a04bad43822ac3ff50e39e'
 };
 
 // Initialize Firebase
