ca8f16c91ff09e1019898e63bbbb27edb18bf846
Galo Benjamín Bello Ponce <15621798-gbbelloponce1@users.noreply.gitlab.com>
2023-09-05 17:26:59 -0300

    Fix navbar button name

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -6,7 +6,7 @@
 
 <div class="navbar bg-base-100">
 	<div class="flex-1">
-		<a class="btn btn-ghost normal-case text-xl" href="/">link-sh.ar</a>
+		<a class="btn btn-ghost normal-case text-xl" href="/">Link Share</a>
 	</div>
 	<div class="flex-none">
 		<div class="dropdown dropdown-end">
